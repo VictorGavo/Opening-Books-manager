8f6c6068edd02a90518975f872cf13a81b34b0ba
VictorGavo <victor.gavojdea@gmail.com>
2025-03-16 14:44:49 -0700

    Changed port and host settings

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ def process_form_data(data):
     create_keep_note(data)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=88, debug=True, ssl_context='adhoc')
+    app.run(host='127.0.0.1', port=5000, debug=True)
